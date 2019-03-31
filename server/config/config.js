404e6c92159be2e2827ec7544b031467478cfb19
Katherine <akumu02@hotmail.com>
2019-03-31 10:52:36 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://kathy:yhd6fsd@cluster0-c9oyb.mongodb.net/Cafe?retryWrites=true';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
